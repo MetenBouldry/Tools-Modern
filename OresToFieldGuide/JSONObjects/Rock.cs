4f649d3a91d695579202c0a69b20cd18dcb6e1b5
Pyritie <pyritie@gmail.com>
2025-04-29 15:22:01 +0100

    whitespace

--- OresToFieldGuide/JSONObjects/Rock.cs
+++ OresToFieldGuide/JSONObjects/Rock.cs
@@ -2,16 +2,16 @@
 
 namespace OresToFieldGuide
 {
-    public class Rock : IDataJsonObject
+	public class Rock : IDataJsonObject
 	{
-        [JsonPropertyName("id")]
-        public required string ID { get; set; }
+		[JsonPropertyName("id")]
+		public required string ID { get; set; }
 
-        [JsonPropertyName("pattern")]
-        public required string Pattern { get; set; }
+		[JsonPropertyName("pattern")]
+		public required string Pattern { get; set; }
 
-        [JsonPropertyName("replaceable_blocks")]
-        public required string[] ReplaceableBlocks { get; set; }
+		[JsonPropertyName("replaceable_blocks")]
+		public required string[] ReplaceableBlocks { get; set; }
 
 		[JsonPropertyName("translations")]
 		public required Dictionary<string, string> Translations { get; set; }
